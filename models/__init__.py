c67fcef3fb2f8759542aa31f4e626f556a2f49cd
Lai Wang <lai@gatech.edu>
2023-04-06 15:12:13 -0400

    add test code

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,3 @@
 from .bird_model import bird_model
 from .mesh_regressor import mesh_regressor
-from .mesh_regressor import load_regressor
+from .mesh_regressor import load_regressor, load_regressor_new
